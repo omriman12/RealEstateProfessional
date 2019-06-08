73f150d2182ee93d7496734da089a263804d47a6
ömriman12 <mriby@gmail.com>
2019-06-08 12:52:02 +0300

    backup english version

---
+++ SouthRealEstateEnglishFresh/Controllers/PropertiesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SouthRealEstate.Models;
+
+namespace SouthRealEstate.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SingleProperty()
+        {
+            return View();
+        }
+    }
+}
