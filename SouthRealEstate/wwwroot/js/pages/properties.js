14e160f56577a86d21a1b36ef19e609b16eb7054
ömriman12 <mriby@gmail.com>
2019-08-02 10:23:58 +0300

    minor

---
+++ SouthRealEstate/wwwroot/js/pages/properties.js
@@ -0,0 +1,6 @@
+﻿
+$(document).ready(function () {
+    $(document).on("click", ".single-featured-property .property-thumb", function () {
+        window.location = "/properties/singleproperty";
+    });
+});
